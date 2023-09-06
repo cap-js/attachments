698c590b0b4fc062a3d7768c96ed555cfe2f6787
Mara Kiefer <mara.kiefer@sap.com>
2023-09-06 15:36:42 +0200

    Remove sample-related content

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,6 +1,7 @@
 const cds = require('@sap/cds')
 
-// Make images available to app
+// Make images available to app under:
+// <APP_URL>/media/filename
 cds.on('bootstrap', async app => {
   app.get('/media/', async (req, res) => {
     const { ObjectStoreService } = cds.services
@@ -13,58 +14,3 @@ cds.on('bootstrap', async app => {
     return res
   })
 })
-
-cds.on('served', async () => {
-  // TODO: Replace this by UI feature later on
-  //const { ObjectStoreService } = cds.services
-  //await ObjectStoreService.uploadBulk()
-
-  // Register on READ handler of annotated service
-  registerHandler(readHandler)
-
-  async function readHandler(req, next) {
-    const data = await next()
-    const origin = req.http.req.headers.origin
-    const ref = getEntityRef() //req.target.projection.from.ref[0];
-
-    if (data.length && ref) {
-      data.map((d, i) => {
-        if (d && d.customer && ref) {
-          const file = `${ref}-${d.customer.ID}.png`
-          Object.assign(d.customer, {
-            avatar: `${origin}/media/?file=${file}`
-          })
-        }
-      })
-    }
-    return data
-  }
-})
-
-function registerHandler(readHandler) {
-  Object.values(cds.services)
-  .forEach(s => {
-    Object.values(s.entities).forEach(e => {
-      const elements = e.elements;
-      Object.entries(elements).forEach(([k, v]) => {
-        if (v['@Core.IsURL'] && !s.kind) {
-          console.log(`> Registering on READ handler @${e.name}.${k}`)
-          s.prepend(() => s.on('READ', readHandler))
-        }
-      })
-    })
-  })
-}
-
-function getEntityRef() {
-  let ref = []
-  Object.values(cds.entities).filter(e => e.compositions).forEach(c => {
-    const elements = c.elements;
-    Object.entries(elements).forEach(([k, v]) => {
-      if (v['@Core.IsURL'] && !c.projection) {
-        ref.push(`${c.name}`)
-      }
-    })
-  })
-  return ref[0]
-}
