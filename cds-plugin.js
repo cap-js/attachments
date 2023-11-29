66ce3147c49f5316ba2418af636ecf1a0eaa6ddb
Mara Kiefer <mara.kiefer@sap.com>
2023-11-29 13:46:03 +0100

    Simplify model

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,55 +1,18 @@
 const cds = require('@sap/cds')
 
-const { CreateHandler, PutHandler, ReadHandler, SaveHandler } = require('./lib')
+const { PutHandler, ReadHandler } = require('./lib')
 const { hasResources } = require('./lib/helpers')
-
-
-cds.on('loaded', async (m) => {
-
-	// Get definitions from Dummy entity in our models
-	const { 'sap.attachments.aspect': aspect } = m.definitions; if (!aspect) return // some other model
-	const { '@UI.Facets': [facet], elements: { attachments } } = aspect
-	attachments.on.pop() // remove ID -> filled in below
-
-
-	for (let name in m.definitions) {
-		const entity = m.definitions[name]
-		// Mark entity with '@attachments: { Image: [], Documents: [] }' which
-		// contains a list of keys of the associated type 'Image' or 'Document'
-		if (hasResources(entity)) {
-
-			const keys = [], { elements: elms } = entity
-			for (let e in elms) if (elms[e].key) keys.push(e)
-
-			// Add association to AttachmentsView
-			const on = [...attachments.on]; keys.forEach((k, i) => { i && on.push('||'); on.push({ ref: [k] }) })
-			const assoc = { ...attachments, on }
-			const query = entity.projection || entity.query?.SELECT
-			if (query) {
-			  (query.columns ??= ['*']).push({ as: 'attachments', cast: assoc })
-			} else {
-			  entity.elements.attachments = assoc
-			}
-
-			// Add UI.Facet for AttachmentsView
-			entity['@UI.Facets']?.push(facet)
-
-		}
-	}
-})
-
 
 cds.on('served', async () => {
 	for (const srv of cds.services) {
 		if (srv instanceof cds.ApplicationService) {
 			for (const entity of Object.values(srv.entities)) {
-				if (entity['@attachments']) {
-					srv.prepend(() => srv.on("READ", ReadHandler))
-					cds.db.before("CREATE", CreateHandler)
-				}
+				//if (hasResources(entity)) {
+				srv.prepend(() => srv.on("READ", ReadHandler))
+				//}
 			}
-			//srv.prepend(() => srv.on("PUT", `${srv.name}.Attachments`, PutHandler))
+			// TODO: Experimental (streams attachments content)
+			srv.prepend(() => srv.on("PATCH", `${srv}.Attachments`, PutHandler))
 		}
 	}
-	cds.db.before("*", SaveHandler)
 })
--- index.cds
+++ index.cds
@@ -1,43 +1,14 @@
 using { cuid, managed } from '@sap/cds/common';
 
 type Image : Composition of sap.attachments.Images;
-type Attachments : Association to sap.attachments.Attachments;
 
 context sap.attachments {
 
-  // Used in cds-plugin.js as template for attachments
-  aspect aspect @(UI.Facets: [{
-    $Type                : 'UI.ReferenceFacet',
-    ID                   : 'AttachmentsFacet',
-    Label                : '{i18n>Attachments}',
-    Target               : 'attachments/@UI.PresentationVariant'
-  }]) {
-    attachments    : Association to many AttachmentsTable
-                     on attachments.object = ID;
-    key ID  : UUID;
-  }
-
-  // Should this be draft enabled?
-  //@odata.draft.enabled
-  entity AttachmentsTable as
-    select from Documents {
-      *,
-      attachments.object as object
-    };
-
   entity Images : cuid, managed, MediaData {}
 
-  entity Documents : cuid, managed, MediaData {
-        note        : String @title: 'Note';
-        attachments : Association to Attachments;
-  }
-
-  entity Attachments : cuid, managed {
+  entity Attachments : cuid, managed, MediaData {
     object : UUID;
-    createdAt : managed:createdAt @title: 'On';
-    createdBy : managed:createdBy @title: 'By';
-    documents : Composition of many Documents
-                on documents.attachments = $self;
+    note      : String @title: 'Note';
   }
 
   type MediaData {
@@ -46,25 +17,5 @@ context sap.attachments {
     mimeType  : String @title: 'Attachment Type' @Core.IsMediaType: true;
     url       : String;
   }
-
-  annotate AttachmentsTable with @(UI: {
-    MediaResource: {
-      Stream: content
-    },
-    PresentationVariant: {
-      Visualizations: ['@UI.LineItem'],
-      SortOrder     : [{
-        Property  : createdAt,
-        Descending: true
-      }],
-    },
-    LineItem: [
-      {Value: content},
-      {Value: createdAt},
-      {Value: createdBy},
-      {Value: note}
-    ],
-    DeleteHidden       : true,
-  });
 
 }
