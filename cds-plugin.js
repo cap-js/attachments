e62f64f61c9bbb257e740ebe3cd71b90641b7778
Mara Kiefer <mara.kiefer@sap.com>
2023-11-29 20:11:48 +0100

    Add more comments

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,17 +1,19 @@
 const cds = require('@sap/cds')
 
 const { SaveHandler, ReadHandler } = require('./lib')
-const { hasResources } = require('./lib/helpers')
+//const { hasResources } = require('./lib/helpers')
 
 cds.on('served', async () => {
 	for (const srv of cds.services) {
 		if (srv instanceof cds.ApplicationService) {
 			for (const entity of Object.values(srv.entities)) {
+				// TODO: Reimplement resource type checks ('Image' or 'Attachments')
+				// for simplified data model
 				//if (hasResources(entity)) {
-				srv.prepend((impl) => impl.on("READ", ReadHandler))
+				  srv.prepend((impl) => impl.on("READ", ReadHandler))
 				//}
 			}
-			// TODO: Experimental (streams attachments content)
+			// TODO: Generalize this for all entities that contain attachments
 			const { Incidents } = srv.entities
 			srv.prepend((impl) => impl.on("SAVE", Incidents, SaveHandler))
 		}
