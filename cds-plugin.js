02042ba4a9fcaa08a454c21b1fa3326256b7a2de
Mara Kiefer <8320933+mnkiefer@users.noreply.github.com>
2023-11-30 07:35:57 +0100

    Adapt to new UI and remove express route `/media` (#10)

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,76 +1,21 @@
 const cds = require('@sap/cds')
 
-const { ReadAttachmentsHandler, ReadImagesHandler } = require('./lib')
-const { connectToAttachmentsService, hasResources } = require('./lib/helpers')
-
-
-cds.on('loaded', async (m) => {
-
-	// Get definitions from Dummy entity in our models
-	const { 'sap.attachments.aspect': aspect } = m.definitions; if (!aspect) return // some other model
-	const { '@UI.Facets': [facet], elements: { attachments } } = aspect
-	attachments.on.pop() // remove ID -> filled in below
-
-
-	for (let name in m.definitions) {
-		const entity = m.definitions[name]
-		// Mark entity with '@attachments: { Image: [], Documents: [] }' which
-		// contains a list of keys of the associated type 'Image' or 'Document'
-		if (hasResources(entity)) {
-
-			const keys = [], { elements: elms } = entity
-			for (let e in elms) if (elms[e].key) keys.push(e)
-
-			// Add association to AttachmentsView
-			const on = [...attachments.on]; keys.forEach((k, i) => { i && on.push('||'); on.push({ ref: [k] }) })
-			const assoc = { ...attachments, on }
-			const query = entity.projection || entity.query?.SELECT
-			if (query) {
-			  (query.columns ??= ['*']).push({ as: 'attachments', cast: assoc })
-			} else {
-			  entity.elements.attachments = assoc
-			}
-
-			// Add UI.Facet for AttachmentsView
-			entity['@UI.Facets']?.push(facet)
-
-		}
-	}
-})
-
-// Independent of the data source (db or remote bucket), stream data
-// behind app '/media' url
-cds.on('bootstrap', async app => {
-	app.get('/media/', async (req, res) => {
-		let ID = req.query.ID;
-		if (ID) {
-			const media_srv = await connectToAttachmentsService()
-			// TODO: Get service dynamically (from keys)
-			const stream = await media_srv.onSTREAM('sap.attachments.Images', ID)
-			if (stream) {
-				res.setHeader('Content-Type', 'application/octet-stream')
-				stream.pipe(res)
-			}
-		}
-		return res
-	})
-})
+const { SaveHandler, ReadHandler } = require('./lib')
+//const { hasResources } = require('./lib/helpers')
 
 cds.on('served', async () => {
 	for (const srv of cds.services) {
 		if (srv instanceof cds.ApplicationService) {
-			let any
 			for (const entity of Object.values(srv.entities)) {
-				if (entity['@attachments']) {
-					any = true
-					// This is needed to append image urls to the data
-					srv.prepend(() => srv.on("READ", ReadImagesHandler))
-				}
+				// TODO: Reimplement resource type checks ('Image' or 'Attachments')
+				// for simplified data model
+				//if (hasResources(entity)) {
+				  srv.prepend((impl) => impl.on("READ", ReadHandler))
+				//}
 			}
-			// This is only needed for extra formatting
-			if (any && srv.entities.AttachmentsView) {
-				srv.prepend(() => srv.on("READ", srv.entities.AttachmentsView, ReadAttachmentsHandler))
-			}
+			// TODO: Generalize this for all entities that contain attachments
+			const { Incidents } = srv.entities
+			srv.prepend((impl) => impl.on("SAVE", Incidents, SaveHandler))
 		}
 	}
 })
--- index.cds
+++ index.cds
@@ -1,72 +1,21 @@
 using { cuid, managed } from '@sap/cds/common';
 
 type Image : Composition of sap.attachments.Images;
-type Attachments : Association to sap.attachments.Attachments;
 
 context sap.attachments {
 
-  // Used in cds-plugin.js as template for attachments
-  aspect aspect @(UI.Facets: [{
-    $Type                : 'UI.ReferenceFacet',
-    ID                   : 'AttachmentsFacet',
-    Label                : '{i18n>Attachments}',
-    Target               : 'attachments/@UI.PresentationVariant',
-    //![@UI.PartOfPreview] : false
-  }]) {
-    // Essentially: Association to many Attachments on attachments.attachmentslist.object = ID;
-    attachments    : Association to many AttachmentsView
-                     on attachments.entityKey = ID;
-    key ID  : UUID;
-  }
+  entity Images : cuid, managed, MediaData {}
 
-  // This is a helper view to flatten the assoc path to the objectKey
-  @readonly
-  view AttachmentsView as
-    select from Documents {
-      *,
-      attachments.entityKey as entityKey
-     };
-
-  entity Images : cuid, managed, MediaData {
-        fileName : String;
-  }
-
-  entity Documents : cuid, managed, MediaData {
-        fileName    : String;
-        title       : String;
-        attachments : Association to Attachments;
-  }
-
-  entity Attachments : cuid, managed {
-    entityKey : UUID @odata.Type:'Edm.String';
-    createdAt : managed:createdAt @title: 'On';
-    createdBy : managed:createdBy @title: 'By';
-    documents : Composition of many Documents
-                on documents.attachments = $self;
+  entity Attachments : cuid, managed, MediaData {
+    object    : UUID; // The object we are attached to
+    note      : String @title: 'Note';
   }
 
   type MediaData {
-    content  : LargeBinary;
-    url      : String  @Core.IsURL: true  @Core.MediaType: mimeType;
-    mimeType : String  @Core.IsMediaType: true;
+    fileName : String;
+    content   : LargeBinary @title: 'Attachment' @Core.MediaType: mimeType @Core.ContentDisposition.Filename: fileName @Core.Immutable: true;
+    mimeType  : String @title: 'Attachment Type' @Core.IsMediaType: true;
+    url       : String;
   }
-
-  annotate AttachmentsView with @(UI: {
-    PresentationVariant: {
-      Visualizations: ['@UI.LineItem#uploadTable'],
-      SortOrder     : [{
-        Property  : createdAt,
-        Descending: true
-      }],
-    },
-    LineItem #uploadTable: [
-      {Value: createdAt},
-      {Value: createdBy},
-      {Value: fileName},
-      {Value: title},
-      {Value: entityKey}
-    ],
-    DeleteHidden       : true,
-  });
 
 }
