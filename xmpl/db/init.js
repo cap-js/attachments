2d7358bd83c3731c86fb6b7dedaf4c9762b906d5
Daniel Hutzel <daniel.hutzel@sap.com>
2023-12-17 15:45:57 +0100

    Using cds.ql with tts

--- xmpl/db/init.js
+++ xmpl/db/init.js
@@ -3,10 +3,10 @@
 const cds = require('@sap/cds')
 cds.once('served', async () => {
   const Attachments = await cds.connect.to('attachments')
-  const attachments = await SELECT `ID,filename`.from('sap.common.Attachments').where({ content: null })
+  const attachments = await SELECT `ID, filename` .from `sap.common.Attachments` .where `content is null`
   await Promise.all (attachments.map(_init))
   await Attachments.upload(attachments)
-  await UPDATE ('ProcessorService.Customers') .set (`avatar_ID = ID`)
+  await UPDATE `ProcessorService.Customers` .set `avatar_ID = ID`
 })
 
 // Helpers...
