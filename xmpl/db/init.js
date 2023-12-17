a3eae1172e44eba3007541eecb1618d99dd600d5
Daniel Hutzel <daniel.hutzel@sap.com>
2023-12-17 15:41:31 +0100

    .

--- xmpl/db/init.js
+++ xmpl/db/init.js
@@ -1,11 +1,14 @@
 const cds = require('@sap/cds')
 cds.once('served', async () => {
-  const { readFile } = require('fs').promises
-  const { join } = require('path')
-  const AttachmentsSrv = await cds.connect.to('attachments')
+  const Attachments = await cds.connect.to('attachments')
   const data = await SELECT`ID,filename`.from('sap.common.Attachments').where({ content: null })
-  // REVISIT: This is not streaming, is it?
-  await Promise.all (data.map (d => readFile(join(__dirname, 'content', d.filename)).then(b => d.content = b)))
-  await AttachmentsSrv.upload(data)
+  await Promise.all (data.map(_init))
+  await Attachments.upload(data)
   await UPDATE ('ProcessorService.Customers') .set (`avatar_ID = ID`)
 })
+
+// Helpers...
+const { join } = require('path'), _content = join(__dirname, 'content')
+const { readFile } = require('fs').promises
+const _init = a => readFile(join(_content, a.filename)).then(c => a.content = c)
+// REVISIT: This ^^^is not streaming, is it?
