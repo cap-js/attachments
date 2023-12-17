7321bf1810dd54911c0d31a8318a3c60e2e9d2bd
Daniel Hutzel <daniel.hutzel@sap.com>
2023-12-17 14:11:44 +0100

    ...

--- xmpl/db/init.js
+++ xmpl/db/init.js
@@ -5,7 +5,7 @@ cds.once('served', async () => {
   const AttachmentsSrv = await cds.connect.to('attachments')
   const data = await SELECT`ID,filename`.from('sap.common.Attachments').where({ content: null })
   // REVISIT: This is not streaming, is it?
-  await Promise.all (data.map (d => readFile(join(__dirname, 'data/media', d.filename)).then(b => d.content = b)))
+  await Promise.all (data.map (d => readFile(join(__dirname, 'content', d.filename)).then(b => d.content = b)))
   await AttachmentsSrv.upload(data)
   await UPDATE ('ProcessorService.Customers') .set (`avatar_ID = ID`)
 })
