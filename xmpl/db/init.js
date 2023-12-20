df6e9294e3a62ace8d544fe0d9163e8a3197219b
Daniel Hutzel <daniel.hutzel@sap.com>
2023-12-20 13:28:07 +0100

    We don't need the db/init for customers

--- xmpl/db/init.js
+++
@@ -1,3 +0,0 @@
-// This is for demo purposes only, not for production use!
-const cds = require('@sap/cds')
-cds.once('served', async () => UPDATE `ProcessorService.Customers` .set `avatar_ID = ID`)
