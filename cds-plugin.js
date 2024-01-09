0b9ec6e632639094587fa2b18dd4ed16642f4b71
Daniel Hutzel <daniel.hutzel@sap.com>
2024-01-09 09:31:00 +0100

    Attachments as compositions of aspect

--- cds-plugin.js
+++ cds-plugin.js
@@ -10,21 +10,11 @@ cds.once('served', ()=> Promise.all([
 
 
 function UnfoldModel (csn) {
-  const Attachments = csn.definitions['sap.common.Attachments']
-  if (Attachments) Attachments._is_attachments = true; else return
-  cds.linked(csn).forall('Composition', comp => {
-    if(comp._target._is_attachments && comp.parent && comp.is2many && !comp.on) {
-      let keys = Object.keys(comp.parent.keys)
-      if (keys.length > 1) throw cds.error `Entities with attachments must have a single key element`
-      // Fill in on condition
-      delete comp.keys
-      comp.on = [
-        {"ref":[ comp.name, 'subject' ]}, '=',
-        {"ref":[ '$self', keys[0] ]}
-      ]
-      // Add UI.Facets
-      let Facets = comp.parent['@UI.Facets']
-      if (Facets) Facets.push({
+  if ('Attachments' in csn.definitions) cds.linked(csn).forall('Composition', comp => {
+    if (comp._target['@_is_media_data'] && comp.parent && comp.is2many) {
+      let Facets = comp.parent['@UI.Facets']; if (!Facets) return
+      DEBUG?.('Adding @UI.Facet to:', comp.parent.name)
+      Facets.push({
         $Type : 'UI.ReferenceFacet', Target: `${comp.name}/@UI.LineItem`,
         Label : '{i18n>Attachments}',
       })
@@ -34,66 +24,65 @@ function UnfoldModel (csn) {
 
 
 async function UploadInitialContent() {
-  const { isdir, local } = cds.utils, _content = isdir('db/content'); if (!_content) return
-  const Attachments = await cds.connect.to('attachments')
-  const { join } = cds.utils.path
-  const { readFile } = cds.utils.promises
-  const _init = a => readFile(join(_content, a.filename)).then(c => a.content = c)
-  // REVISIT: This ^^^ is not streaming, is it?
-  const attachments = await SELECT `ID, filename` .from `sap.common.Attachments` .where `content is null`
-  if (!attachments.length) return
-  LOG.info('Loading initial content from', local(_content))
-  await Promise.all (attachments.map(_init))
-  await Attachments.upload(attachments)
+  let { isdir, local } = cds.utils, _content = isdir('db/content'); if (!_content) return
+  let { join } = cds.utils.path, { readFile } = cds.utils.fs.promises
+  let AttachmentsSrv = await cds.connect.to('attachments'), n=0
+  for (let each of cds.model.each (d => d['@_is_media_data'] && d.keys && !d.query)) {
+    if (!n++) DEBUG?.('Uploading initial content from:', local(_content), '...')
+    let keys = Object.keys(each.keys)
+    let attachments = await SELECT(...keys,'content').from(each).where({ content: { like: 'file:%' } })
+    if (!attachments.length) continue
+    await Promise.all (attachments.map (a => readFile (join(_content, a.filename ??= a.content.slice(5))).then(c => a.content = c)))
+    await AttachmentsSrv.put(each,attachments)
+  }
 }
 
 
 async function PluginHandlers () {
-  const Attachments = cds.model.definitions['sap.common.Attachments']
-  if (Attachments) Attachments._is_attachments = true; else return
 	await cds.connect.to('attachments') // ensure to connect to the attachments service
-	for (const srv of cds.services) {
+  const Images = cds.model.definitions['sap.common.Images']
+  Images._is_images = true
+	for (let srv of cds.services) {
 		if (srv instanceof cds.ApplicationService) {
 			Object.values(srv.entities) .forEach (entity => {
 				let any=0; for (let e in entity.elements) {
 					if (e === 'SiblingEntity') continue // REVISIT: Why do we have this?
-					const element = entity.elements[e]
-					if (element._target?._is_attachments) {
-						DEBUG?.('serving attachments for:', `${entity.name}/${e}`)
-						srv.prepend(() => {
-							srv.on ("READ", `${entity.name}/${e}`, ReadAttachmentsHandler)
-							srv.on ("READ", entity.name +'/'+ element.name, ReadAttachmentsHandler)
-						})
+					const element = entity.elements[e], target = element._target
+					if (target?.['@_is_media_data']) {
+						DEBUG?.('serving attachments for:', target.name)
+            srv.prepend(() => {
+              const handler = target._is_images ? ReadImage : ReadAttachment
+              for (let each of [target, target.drafts]) if (each) srv.on ("READ", each, handler)
+            })
+            srv.after ("SAVE", entity, DraftSaveHandler4(element))
 						any++
 					}
 				}
-				if (any) srv.prepend(() => {
-					srv.on ("READ", entity, ReadHandler)
-					srv.on ("SAVE", entity, SaveHandler)
-				})
+				if (any) srv.prepend(() => srv.on ("READ", entity, ReadHandler))
 			})
 		}
 	}
 }
 
 
-async function ReadAttachmentsHandler (req, next) {
+async function ReadImage (req, next) {
+  if (!req._path?.endsWith('$value')) return next()
+  let keys = { ID: req.params.at(-1) }
+  let stream = await cds.services.attachments.get (req.target,keys)
+  return { value: stream } // REVISIT: where does that { value: ... } come from? OData?
+}
 
-  // Add image streaming
-  const image_requested = req._path?.endsWith('$value')
-  if (image_requested) {
-    const stream = await cds.services.attachments.download(req.params[0])
-    return { value: stream }
+async function ReadAttachment (req, next) {
+  if (!req._path?.endsWith('content')) return next()
+  let Attachments = req.target, keys = req.params.at(-1)
+  if (Attachments.isDraft) {
+    // New attachements added in draft mode are stored in the database...
+    let stream = await STREAM.from (Attachments,keys) .column ('content')
+    if (stream) return { value: stream } // REVISIT: where does that { value: ... } come from? OData?
+    else Attachments = Attachments.actives // if not found, read from active data subsequently...
   }
-
-  // Add attachment streaming (i.e. for S3 bucket)
-  const attachment_requested = req._path?.endsWith('content') && !req.entity.endsWith('.drafts')
-  if (attachment_requested) {
-    const stream = await cds.services.attachments.download(req.data.ID)
-    return { value: stream }
-  }
-
-  else return next()
+  let stream = await cds.services.attachments.get (Attachments,keys)
+  return { value: stream } // REVISIT: where does that { value: ... } come from? OData?
 }
 
 
@@ -120,14 +109,26 @@ async function ReadHandler (req, next) {
 }
 
 
-const SaveHandler = async function (req, next) {
-  // Copy attachments from draft to active
-  const results = await next()
+/**
+ * Returns a handler to copy updated attachments content from draft to active / object store
+ */
+const DraftSaveHandler4 = composition => {
   const AttachmentsSrv = cds.services.attachments
-  const { Attachments } = this.entities
-  // REVISIT: This is loading the attachments into buffers -> needs streaming instead
-  // REVISIT: This is always fetching and re-uploading all attachments from draft data -> needs to be optimized
-  const attachments = await SELECT`ID, filename, content`.from(Attachments.drafts).where({ subject: req.data.ID })
-  await Promise.all (attachments.map (a => a.content && AttachmentsSrv.upload(a)))
-  return results
+  const Attachments = composition._target
+  const keys_and_content = _keys4(Attachments) .map (k => ({ref:[k]})) .concat ({ref:['content']})
+  return async (_, req) => {
+    // REVISIT: The below query loads the attachments into buffers -> needs streaming instead
+    const attachments = await SELECT (keys_and_content) .from (Attachments.drafts) .where ([
+      ...req.subject.ref[0].where.map(x => x.ref ? {ref:['up_',...x.ref]} : x),
+      'and', {ref:['content']}, 'is not null' // NOTE: needs skip LargeBinary fix to Lean Draft
+    ])
+    if (attachments.length) await AttachmentsSrv.put (Attachments, attachments)
+  }
+}
+
+
+const _keys4 = Attachments => {
+  let { up_ } = Attachments.keys
+  if (up_) return up_.keys.map(k => 'up__'+k.ref[0]).concat('filename')
+  else return Object.keys(Attachments.keys)
 }
