2eb61c5fe3250318f1204e94e758f0190b35a716
Muskan Sethi <muskan.sethi@sap.com>
2024-02-26 14:53:25 +0530

    initial test setup

---
+++ test/attachments.test.js
@@ -0,0 +1,30 @@
+const AttachmentsPlugin = require("../lib/plugin")
+const cds = require('@sap/cds/lib')
+
+describe("attachments tests", () => {
+    
+    //Uploading images and testing the url
+    it("Uploading and reading the image", async () => {
+
+    });
+
+    //Uploading attachment document and testing the url
+    it("Uploading and reading the attachment document", async () => {
+
+    });
+
+    //Uploading attachment image and testing the url
+    it("Uploading and reading the attachment image", async () => {
+
+    });
+
+    //Deleting the attachment
+    it("Deleting the attachment", async () => {
+
+    });
+
+    //Test to check s3 api
+    it("Checking s3 api", async () => {
+
+    });
+})
