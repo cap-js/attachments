ab32224eb2a4a46b3e73f1358c00b0b53230c534
Muskan Sethi <muskan.sethi@sap.com>
2024-02-27 13:05:37 +0530

    added initial data mimetype

--- xmpl/db/init.js
+++ xmpl/db/init.js
@@ -6,13 +6,13 @@ module.exports = async function () {
 
   const { 'sap.capire.incidents.Incidents.attachments': Attachments } = cds.model.entities
   await attachments.put (Attachments, [
-    [ '3b23bb4b-4ac7-4a24-ac02-aa10cabd842c', 'INVERTER FAULT REPORT.pdf' ],
-    [ '3b23bb4b-4ac7-4a24-ac02-aa10cabd842c', 'Inverter-error-logs.txt' ],
-    [ '3a4ede72-244a-4f5f-8efa-b17e032d01ee', 'No_Current.xlsx' ],
-    [ '3ccf474c-3881-44b7-99fb-59a2a4668418', 'strange-noise.csv' ],
-    [ '3583f982-d7df-4aad-ab26-301d4a157cd7', 'Broken Solar Panel.jpg' ]
-  ].map(([ up__ID, filename ]) => ({
-    up__ID, filename,
+    [ '3b23bb4b-4ac7-4a24-ac02-aa10cabd842c', 'INVERTER FAULT REPORT.pdf', 'application/pdf' ],
+    [ '3b23bb4b-4ac7-4a24-ac02-aa10cabd842c', 'Inverter-error-logs.txt', 'application/txt' ],
+    [ '3a4ede72-244a-4f5f-8efa-b17e032d01ee', 'No_Current.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ],
+    [ '3ccf474c-3881-44b7-99fb-59a2a4668418', 'strange-noise.csv', 'text/csv' ],
+    [ '3583f982-d7df-4aad-ab26-301d4a157cd7', 'Broken Solar Panel.jpg', 'image/jpeg' ]
+  ].map(([ up__ID, filename, mimeType ]) => ({
+    up__ID, filename, mimeType, 
     content: createReadStream (join(__dirname, 'content', filename)),
     createdAt: new Date (Date.now() - Math.random() * 30*24*60*60*1000),
     createdBy: 'alice',
