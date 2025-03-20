f23e648e05f950e7ccc51f3f61705dd6fd313aad
Jeevitha P <jeevitha.p@sap.com>
2025-03-20 17:04:58 +0530

    minor refactoring

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,5 +1,2 @@
-const cds = require("@sap/cds");
 require('./lib/plugin')
-if(cds.env.profile =='with-mtx-sidecar' && cds.env.requires.attachments.objectstore.kind=="separate"){
 require('./lib/mtx/server')
-}
