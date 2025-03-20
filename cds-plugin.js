77032f6496481fd282ea0961c9b22ee1c9d42ef9
Kriti Shekhar <kriti.shekhar@sap.com>
2025-03-20 09:00:09 +0530

    adding debug instead of console log in aws.js file

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,6 +1,5 @@
 const cds = require("@sap/cds");
 require('./lib/plugin')
-console.log("Before mtx")
-if(cds.env.requires.attachments.objectstore.kind=="separate")
+if(cds.env.profile =='with-mtx-sidecar' && cds.env.requires.attachments.objectstore.kind=="separate"){
 require('./lib/mtx/server')
-console.log("After mtx")
+}
