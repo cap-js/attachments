3f960d7f716ee96172d73140bab53b9dfacbdf6e
SoujitD-SAP <soujit.das@sap.com>
2025-03-21 11:37:37 +0530

    Added dependency of @cap-js/cds-test
    due to failing test cases added required dependency

--- tests/integration/attachments-non-draft.test.js
+++ tests/integration/attachments-non-draft.test.js
@@ -1,7 +1,7 @@
 const cds = require("@sap/cds");
 const path = require("path");
 const app = path.resolve(__dirname, "../incidents-app-non-draft");
-const { expect, axios } = cds.test(app);
+const { expect, axios } = require("@cap-js/cds-test")(app);
 const { createReadStream } = cds.utils.fs;
 const { join } = cds.utils.path;
 
