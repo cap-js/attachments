34b4cc7ad72029181db36231d603e63498d5d321
I569613 <marvin.lindner@sap.com>
2025-10-08 12:47:53 +0200

    jest now exits after the tests and doesnt wait

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 const config = {
     testTimeout: 42222,
     testMatch: ['**/*.test.js'],
-    setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
+    setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+    forceExit: true,
+    detectOpenHandles: true
   }
 
 module.exports = config
