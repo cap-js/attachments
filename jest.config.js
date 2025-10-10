6dfa7c461092014a9c5b8b33195437332d16b5de
Marvin <marvin.lindner@sap.com>
2025-10-10 15:07:20 +0200

    258 chore remove cross dependency from tests (#259)
    This pr:
    - changes the attachment tests so they aren't dependent on one another
    - added a clearing of the attachments before and after each test
    - added helper functions to reduce redundant code
    - documented these helper functions if they are ever needed again
    
    Closes: #258
    
    ---------
    
    Co-authored-by: Marten Schiwek <marten.schiwek@sap.com>
    Co-authored-by: Simon Kobler <32038731+KoblerS@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 const config = {
-    testTimeout: 42222,
+    testTimeout: 60000,
     testMatch: ['**/*.test.js'],
-    setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
+    setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+    forceExit: true,
+    detectOpenHandles: true
   }
 
 module.exports = config
--- tests/utils/api.js
+++ tests/utils/api.js
@@ -2,34 +2,34 @@ class RequestSend {
   constructor(post) {
     this.post = post
   }
-  async draftModeActions(
-    serviceName,
-    entityName,
-    id,
-    path,
-    action,
-    isRootCreated = false
-  ) {
-    if (!isRootCreated) {
-      try {
-        await this.post(
-          `odata/v4/${serviceName}/${entityName}(ID=${id},IsActiveEntity=true)/${path}.draftEdit`,
-          {
-            PreserveChanges: true,
-          }
-        )
-      } catch (err) {
-        return err
-      }
-    }
+  async draftModeEdit(serviceName, entityName, id, path) {
     try {
+      // Create draft from active entity
+      await this.post(
+        `odata/v4/${serviceName}/${entityName}(ID=${id},IsActiveEntity=true)/${path}.draftEdit`,
+        {
+          PreserveChanges: true,
+        }
+      )
+    } catch (err) {
+      return err
+    }
+  }
+
+  async draftModeSave(serviceName, entityName, id, action, path) {
+    try {
+      // Execute the action (e.g., POST attachment)
       await action()
+
+      // Prepare the draft
       await this.post(
         `odata/v4/${serviceName}/${entityName}(ID=${id},IsActiveEntity=false)/${path}.draftPrepare`,
         {
           SideEffectsQualifier: "",
         }
       )
+
+      // Activate the draft
       await this.post(
         `odata/v4/${serviceName}/${entityName}(ID=${id},IsActiveEntity=false)/${path}.draftActivate`,
         {}
