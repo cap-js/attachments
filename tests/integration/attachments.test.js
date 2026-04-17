e622052070553214776f16eb57a7a240992d8ece
Eric Peairs <eric.peairs@sap.com>
2026-04-17 11:35:34 +0200

    Merge conflict

--- tests/integration/attachments.test.js
+++ tests/integration/attachments.test.js
@@ -644,7 +644,7 @@ describe("Tests for uploading/deleting attachments through API calls", () => {
       'The size of "toobig.txt" exceeds the maximum allowed limit of 1KB',
     )
   })
-  
+
   it("Calling a CAP action via srv.send does not crash handlers", async () => {
     const srv = await cds.connect.to("ProcessorService")
 
